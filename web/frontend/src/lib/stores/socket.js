d94bd3c9a8bd2af74ef216fe8ded442447b07e77
WadyAldy <98575749+WadyAldy@users.noreply.github.com>
2026-02-10 21:18:31 +0530

    Update socket.js

--- web/frontend/src/lib/stores/socket.js
+++ web/frontend/src/lib/stores/socket.js
@@ -1,3 +1,17 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
+import { io } from "socket.io-client";
 
 export const socket = writable(null);
+
+export function initSocket(username) {
+  const s = io("https://<YOUR-RENDER-APP>.onrender.com", {
+    transports: ["websocket"],
+    auth: {
+      username
+    }
+  });
+
+  socket.set(s);
+
+  return s;
+}
